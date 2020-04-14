2e8f6779dd28dfe50504001953b825e86659ad80
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-04-14 11:33:29 +0800

    good enough

---
+++ src/components/imglinkform/ImgLinkForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './ImgLinkForm.css'
+
+
+const ImgLinkForm = ({ onInputChange, onButtonSubmit }) => {
+	return (
+		<div>
+			<p className='f3'>
+				{'This Smart Brain will detect faces in your pictures. '}
+			</p>
+			<div className='center'>
+				<div className='form center pa4 br3 shadow-5'>
+					<input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+						<button 
+						className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+						onClick={onButtonSubmit}>
+						Detect</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default ImgLinkForm;
